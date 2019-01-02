04dd60bd9aaf303f23a32094f3140084f0478001
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-02 22:56:10 +0000

    update functioNames

--- client/src/helpers/functionNames.ts
+++ client/src/helpers/functionNames.ts
@@ -4,37 +4,16 @@ interface IFunctionNamesDict {
 
 const functionNames: IFunctionNamesDict = {
   0xa9059cbb: "Transfer",
-  0x038b1350: "",
   0x363349be: "FillOrdersUpTo",
-  0x01012300: "",
-  0x96b5a755: "",
-  0xadf2cead: "",
-  0x3d7d3f5a: "",
-  0x9d065838: "",
-  0x83f12fec: "",
-  0x57111a75: "",
-  0xa438545b: "",
-  0xa40c505e: "",
   0x42966c68: "Burn",
   0x454a2ab3: "Bid",
-  0x78053700: "",
-  0x6b1d4db7: "",
   0x4f150787: "BatchFillOrKillOrders",
-  0x3d8d020a: "",
-  0x1e89d545: "",
   0x23b872dd: "TransferFrom",
-  0xed74de9d: "",
-  0x975ef7df: "",
-  0x7e9e511d: "",
-  0x04f3c40e: "",
-  0xae57de0c: "",
-  0x232a2c1d: "",
-  0xd2874e49: "",
-  0x1d4d691d: "",
+  0xd0e30db0: "Deposit",
+  0x095ea7b3: "Approve",
+  0x2e1a7d4d: "Withdraw",
   0xbc61394a: "FillOrder",
-  0x8ba4cc3c: "",
-  0xc01a8c84: "ConfirmTransaction",
-  0x67243482: ""
+  0xc01a8c84: "ConfirmTransaction"
 };
 
 export default functionNames;
